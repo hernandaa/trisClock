915a587c2bdffb4cd91e10969cdaffbb6e244000
Hernanda <42990222+hernandaa@users.noreply.github.com>
2019-01-03 14:13:26 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace trisClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockLabel = new System.Windows.Forms.Label();
+            this.addHrTextBox = new System.Windows.Forms.TextBox();
+            this.AddMtTextbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.addScTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clockLabel
+            // 
+            this.clockLabel.AutoSize = true;
+            this.clockLabel.Font = new System.Drawing.Font("Prestige Elite Std", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLabel.Location = new System.Drawing.Point(17, 9);
+            this.clockLabel.Name = "clockLabel";
+            this.clockLabel.Size = new System.Drawing.Size(704, 151);
+            this.clockLabel.TabIndex = 0;
+            this.clockLabel.Text = "00:00:00";
+            // 
+            // addHrTextBox
+            // 
+            this.addHrTextBox.Font = new System.Drawing.Font("Prestige Elite Std", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addHrTextBox.Location = new System.Drawing.Point(43, 163);
+            this.addHrTextBox.Name = "addHrTextBox";
+            this.addHrTextBox.Size = new System.Drawing.Size(150, 49);
+            this.addHrTextBox.TabIndex = 1;
+            // 
+            // AddMtTextbox
+            // 
+            this.AddMtTextbox.Font = new System.Drawing.Font("Prestige Elite Std", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddMtTextbox.Location = new System.Drawing.Point(293, 163);
+            this.AddMtTextbox.Name = "AddMtTextbox";
+            this.AddMtTextbox.Size = new System.Drawing.Size(150, 49);
+            this.AddMtTextbox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Rousseau Deco", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(613, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 49);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // addScTextbox
+            // 
+            this.addScTextbox.Font = new System.Drawing.Font("Prestige Elite Std", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addScTextbox.Location = new System.Drawing.Point(533, 163);
+            this.addScTextbox.Name = "addScTextbox";
+            this.addScTextbox.Size = new System.Drawing.Size(150, 49);
+            this.addScTextbox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Prestige Elite Std", 26.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(225, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 40);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Prestige Elite Std", 26.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(472, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 40);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ":";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 318);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addScTextbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.AddMtTextbox);
+            this.Controls.Add(this.addHrTextBox);
+            this.Controls.Add(this.clockLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clockLabel;
+        private System.Windows.Forms.TextBox addHrTextBox;
+        private System.Windows.Forms.TextBox AddMtTextbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox addScTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trisClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.setInitial();
+        }
+
+        protected void setInitial()
+        {
+            clockLabel.Text = DateTime.Now.ToString("hh") + ":" + DateTime.Now.ToString("mm") + ":" + DateTime.Now.ToString("ss");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] time = this.clockLabel.Text.Split(':');
+            int tick = Convert.ToInt16(time[2]) + 1;
+            time[2] = tick.ToString();
+            this.clockLabel.Text = string.Join(":", time);
+
+            countingTime(time);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] time = this.clockLabel.Text.Split(':');
+
+            int sec = (this.addScTextbox.Text == "") ?
+                Convert.ToInt32(time[2]) : Convert.ToInt16(time[2]) + Convert.ToInt16(this.addScTextbox.Text);
+            int min = (this.AddMtTextbox.Text == "") ?
+                Convert.ToInt32(time[1]) : Convert.ToInt16(time[1]) + Convert.ToInt16(this.AddMtTextbox.Text);
+            int hr = (this.addHrTextBox.Text == "") ?
+                Convert.ToInt32(time[0]) : Convert.ToInt16(time[0]) + Convert.ToInt16(this.addHrTextBox.Text);
+
+            //int test = Convert.ToInt16(time[1]) + Convert.ToInt16(this.AddMtTextbox.Text);
+
+            this.clockLabel.Text = hr.ToString("00") + ":" + min.ToString("00") + ":" + sec.ToString("00");
+            countingTime(this.clockLabel.Text.Split(':'));
+        }
+
+        protected void countingTime(string[] time)
+        {
+            double sec = Convert.ToInt32(time[2]);
+            double min = Convert.ToInt32(time[1]);
+            double hr = Convert.ToInt32(time[0]);
+
+            if(sec > 59)
+            {
+                double fsec = sec / 60;
+                min = min + Convert.ToInt16(sec / 60);
+                sec = (int)fsec;
+            }
+
+            if(min > 59)
+            { 
+                double fmin = min / 60;
+                hr = min + Convert.ToInt16(hr / 60);
+                min = (int)fmin;
+            }
+
+            if(hr > 23)
+            {
+                double fhr = hr / 24;
+                hr = fhr - Math.Truncate(fhr);
+            }
+
+            this.clockLabel.Text = hr.ToString("00") + ":" + min.ToString("00") + ":" + sec.ToString("00");
+        }
+    }
+}
